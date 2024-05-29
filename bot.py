468812f98427157696e185e3fdde250d000001e5
Mandi Coder <mandicoder@proton.me>
2024-05-29 23:19:55 +0000

    server add

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 from modules.Client import app as bot
 from modules.DownloadPhotos import DownloadPhotos
+from modules.custom_server import handle
 from pyrogram import filters
+from pyrogram.methods.utilities.idle import idle
+from aiohttp import web
+
 
 
 @bot.on_message(filters.command("start"))
@@ -8,17 +12,26 @@ def command_start(app, msg):
     msg.reply("Hola")
 
 
-@bot.on_message(filters.command("run"))
-def upload_images(app, msg):
-    DownloadPhotos(app, f'https://hentaidad.com/popular/{1}').get_pages()
-
-
-
 @bot.on_message()
 def ver_id(app, msg):
     msg.reply( f'`{msg.chat.id}`' )
     
 
+
+async def run_server():
+    print("SERVER INICIADO")
+    server = web.Application()
+    server.add_routes([web.get('/', handle)])
+    runner = web.AppRunner(server)
+
+    await runner.setup()
+    await web.TCPSite(runner, host='0.0.0.0', port=8000).start()
+    
+    await bot.start()
+    
+
 if __name__ == '__main__':
-    print("BOT INICIADO")
-    bot.run()
+    # bot.loop.run_until_complete(run_server())
+    # idle()
+    bot.start()
+    DownloadPhotos(bot, f'https://hentaidad.com/').get_pages()
--- modules/DownloadPhotos.py
+++ modules/DownloadPhotos.py
@@ -2,8 +2,9 @@ from os import makedirs
 from os.path import basename, join, exists
 from requests import get
 from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from modules.Client import ID_CHANNEL
+from time import sleep
+from pyrogram.errors import FloodWait
 
 class DownloadPhotos:
     def __init__(self, app, url:str=None, folder:str="img") -> None:
@@ -13,23 +14,8 @@ class DownloadPhotos:
     def download_images(self, list_img:list, name:str, tags:str):
         caption = name.replace('-', ' ')
         print(f"Descargando: {caption}\nCantidad de Imagenes: {len(list_img)}", flush=True)
-        # img_send = []
-        with ThreadPoolExecutor() as executor:
-            futuros = []
-            for img in list_img:
-                futuros.append(
-                    executor.submit(
-                        self.download,
-                        img,
-                        f"__**{name}**__\n\n__{tags}__"
-                    )
-                )
-                
-                
-    def upload_photo(self, url:str, name:str, tags:str):
-        print("Subiendo: ", name)
-        
-        
+        for img in list_img:
+            self.download(img, f"__**{name}**__\n\n__{tags}__")
 
     
     def download(self, url, caption):
@@ -42,7 +28,14 @@ class DownloadPhotos:
         with open(join(folder_path, name), 'wb') as img:
             print(f"\33[1;32mDescargando: \33[35m{name}\33[0m", flush=True)
             img.write( get(url).content )
-            self.app.send_photo(-1002011762768, url, caption=caption)
+            sleep(0.50)
+            try:
+                self.app.send_photo(-1002011762768, url, caption=caption)
+            except FloodWait as e:
+                print(f"Debes esperar {e.value} segundos.")
+                sleep(e.value)
+                self.app.send_photo(-1002011762768, url, caption=caption)
+
             return join(folder_path, name)
         
     
@@ -64,7 +57,6 @@ class DownloadPhotos:
             link = str(element).split('<a href="')[-1].split('"')[0]
             if link.startswith('http'):
                 list_img.append(link)
-                
         return list_img, tags
             
             
@@ -75,20 +67,25 @@ class DownloadPhotos:
         else:
             print(f"Error al obtener la página: {response.status_code}")
         
-        
-        
     def get_pages(self):
         elements = self.get_soup(self.url).find_all(class_='col-md-3 ajax-load')
-        with ThreadPoolExecutor() as executor:
-            futuros = []
-            for element in elements:
-                futuros.append( executor.submit(self.get_links, element) )
+        for element in elements:
+            enlace = self.get_links(element)
+            name = basename(enlace)
+            data_img = self.get_images( enlace )
+            self.download_images(data_img[0], name, data_img[1])
+            
 
-            for futuro in as_completed(futuros):
-                enlace = futuro.result()
-                name = basename(enlace)
-                try:
-                    data_img = self.get_images( enlace )
-                    self.download_images(data_img[0], name, data_img[1])
-                except Exception as e:
-                    print(e)
+        # with ThreadPoolExecutor() as executor:
+        #     futuros = []
+        #     for element in elements:
+        #         futuros.append( executor.submit(self.get_links, element) )
+
+        #     for futuro in as_completed(futuros):
+        #         enlace = futuro.result()
+        #         name = basename(enlace)
+        #         try:
+        #             data_img = self.get_images( enlace )
+        #             self.download_images(data_img[0], name, data_img[1])
+        #         except Exception as e:
+        #             print(e)
